cb0702bb68b2b9ed91b7b742fcba715c9cac4b33
fairoos <fairoosnm123@gmail.com>
2019-02-05 17:14:41 +0530

    third commit ex3....

---
+++ ex3.2.py
@@ -0,0 +1,9 @@
+print ("How many balls are there?")
+print ("Here", 3 + 3, "Red balls and",2 + 3, "blue balls")
+
+print ("oh, Nice")
+
+print ("make sense True or False")
+
+print ("blue balls are more in basket:", 2 + 3 > 3 )
+
