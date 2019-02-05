34888203ecb7087f37be2422b3bf7ca11ac503ff
fairoos <fairoosnm123@gmail.com>
2019-02-05 17:12:58 +0530

    second commit added .gitignore

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A commit, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print ("I could have code like this.") # and the commend after is ignored.
+
+# You can also use a comment to "disable" orcomment out code.
+#print ("This won't work.")
+
+print ("This will run.")
