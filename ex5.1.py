b1ee62ae32d50ed1d56c78002ecc0587e09e166f
fairoos <fairoosnm123@gmail.com>
2019-02-06 20:03:03 +0530

    commited with ex5

---
+++ ex5.1.py
@@ -0,0 +1,10 @@
+my_name = 'fairoos'
+my_hight = 70 # in cm
+my_weight = 59 # in kg
+
+print(f"I am {my_name} .")
+cm_to_inches = my_hight * 2.54
+kg_to_pounds = my_weight * 2.205
+
+print(f"I have {my_hight} cm hight, that is {cm_to_inches} inches.")
+print(f"I have {my_weight} kg weight, that is {kg_to_pounds} pounds.")
