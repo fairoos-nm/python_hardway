830327ac25cbdb8443d840fe09a4d251e1c59a0b
fairoos <fairoosnm123@gmail.com>
2019-02-08 22:26:28 +0530

    ex10.0 and format.py

---
+++ format.py
@@ -0,0 +1,11 @@
+a = "i"
+b = "love"
+c = "coding"
+d = "math"
+e = "sleep"
+hello = "{} {} {} {}"
+print(hello.format("if you love", c ,"it is very nice", "\nlove math too"))
+math = f"\t{a} {b} {d}."
+print(math)
+
+print(" \bcoding is very {}.".format(b))
