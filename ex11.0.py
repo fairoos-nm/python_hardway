8f8540bed44f919a55eac5653277ceceafd896a2
fairoos <fairoosnm123@gmail.com>
2019-02-09 14:02:55 +0530

    11th ex commited

---
+++ ex11.0.py
@@ -0,0 +1,7 @@
+print("how old are you: ", end= '')
+age = input()
+print("how tall are you ", end='')
+height = input()
+print("How much do you weight ",end='')
+weight = input()
+print(f"so, you're {age} old, {height} tall and {weight} heavy.")
