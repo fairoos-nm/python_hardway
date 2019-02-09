ab3c79788d4861c97966da3be6bb1a0321cb4367
fairoos <fairoosnm123@gmail.com>
2019-02-09 14:05:54 +0530

    12ex commit
    input('')

---
+++ ex12.0.py
@@ -0,0 +1,6 @@
+name = input("Enter your name.")
+hight = input("Enter your hight.")
+weight = input("Enter your weight.")
+
+print("hi, {} ,your hight is, {} ,and weight is, {} ,.".format(name, hight, weight))
+print(f"\thi ,{name}, you are helthy now \nbecoz your hight ,{hight}, and weight are {weight }")
