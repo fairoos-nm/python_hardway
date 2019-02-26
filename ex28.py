e9998381ed7dad3dbdc383f53dbd5230a299c691
fairoos <fairoosnm123@gmail.com>
2019-02-26 12:59:30 +0530

    Boolean Practice

---
+++ ex28.py
@@ -0,0 +1,19 @@
+True and True #True
+False and True # False
+1 ==1 and 2 == 1 #False
+"test" == "test" #True
+True and 1 == 1 #True
+False and 0 != 0 #False
+True or 1 == 1 #True
+"test" == "testing" #False
+1 != 0 and 2 == 1 #False
+"test" == 1 #false
+not(True and False) #True
+not(1 ==1 and 0 != 1) #False
+not(10 == 1 or 1000 == 1000) #False
+not(1 != 10 or 3 == 4) #False
+not("testing" == "testing" and "Zed" == "cool") #True
+1 == 1 and (not("testing" == "testing" and "Zed" == "Cool Guy")) #True
+"chunky" == "bancon" and (not(3 == 4 or 3 == 3)) #False
+3 == 3 and (not("testing" == "testing" or "Pyhton" == "Fun")) #False
+
