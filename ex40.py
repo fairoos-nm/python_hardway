214555a3a5cb4b1ab81fda8fdb19ac94b16ab300
fairoos <fairoosnm123@gmail.com>
2019-04-20 12:07:52 +0530

    Adds ex40 , python Modules, class , and objects

---
+++ ex40.py
@@ -0,0 +1,20 @@
+#Module class and objects.
+
+#Modules are like dictionaries
+
+mystuff = {"apple": "I love apples"}
+
+print(mystuff["apple"])
+
+#here we can call a value in dictionaries using thair keys:
+#Assume that you have file mystuff.py and it containing a function called apple:
+
+# def apple():
+#     print("I love apples")
+# my_name = "name"
+
+#You can call it like this function from some where like
+
+#import mystuff.py
+#mystuff.apple()
+#print(mystuff.my_name)
